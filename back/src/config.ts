1356dd28c8a5c4c3d100322c854c0e05d7fa0a7c
Ikuzen <yannis.samba@gmail.com>
2020-06-11 14:34:18 +0200

    replace local db with cloud db

--- back/src/config.ts
+++ back/src/config.ts
@@ -1,10 +1,10 @@
 export const dbConfigTournament = {
-    mongodbUrl: 'mongodb://localhost:27017',
+    mongodbUrl: 'mongodb+srv://Ikuzen:HBBM5uidbQKLkXRX@cluster0-7rybn.mongodb.net/test',
     dbName: 'TournamentDB',
     collectionName: 'Tournament',
 }
 export const dbConfigUser = {
-    mongodbUrl: 'mongodb://localhost:27017',
+    mongodbUrl: 'mongodb+srv://Ikuzen:HBBM5uidbQKLkXRX@cluster0-7rybn.mongodb.net/test',
     dbName: 'TournamentDB',
     collectionName: 'User',
 }
