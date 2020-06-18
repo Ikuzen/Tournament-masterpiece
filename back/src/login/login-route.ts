21cc301425570796dd28c27aaf4e049ee38f9a56
Ikuzen <yannis.samba@gmail.com>
2020-06-18 10:29:47 +0200

    change query for login

--- back/src/login/login-route.ts
+++ back/src/login/login-route.ts
@@ -8,25 +8,23 @@ const loginRouter = express.Router();
 loginRouter.post("/", async (req, res) => {
     const { username, password } = req.body;
     try {
-        const users = await UserModel.find().exec();
-        for (let user of users) {
-            if (username == user.username && password == user.password ) {
-                let token = jwt.sign({ _id: user._id, username: user.username }, "kRRorxJyC1pUVDpFldyGz1jRPt8koOyj1xdHF9zxnvht2D1iwOXZDhBQdOKakJOc", { expiresIn: 129600 }); 
-                res.json({
-                    sucess: true,
-                    err: null,
-                    token
-                });
-                break;
-            }
-            else {
-                res.status(401).json({
-                    sucess: false,
-                    token: null,
-                    err: 'Username or password is incorrect'
-                });
-            }
+        const user = await UserModel.findOne({username:username, password:password}).exec();
+        if (user) {
+            let token = jwt.sign({ _id: user._id, username: user.username }, "kRRorxJyC1pUVDpFldyGz1jRPt8koOyj1xdHF9zxnvht2D1iwOXZDhBQdOKakJOc", { expiresIn: 250000 }); 
+            res.json({
+                sucess: true,
+                err: null,
+                token
+            });
         }
+        else {
+             res.status(401).json({
+                sucess: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    
     } catch (error) {
         res.status(500).send(error);
     }
