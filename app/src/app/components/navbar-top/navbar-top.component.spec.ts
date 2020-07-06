71e7d9b3a07ec1ababd34b72394f38cb57cb76c5
yannis <yannis.samba@gmail.com>
2020-07-06 18:50:15 +0200

    adding tests

--- app/src/app/components/navbar-top/navbar-top.component.spec.ts
+++ app/src/app/components/navbar-top/navbar-top.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarTopComponent } from './navbar-top.component';
+import { ComponentsModule } from '../components.module';
+import {RouterTestingModule} from 'node_modules/@angular/router/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { reducer } from 'src/app/reducers/login-page.reducer';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
+import { ToastService } from '../../shared/services/toast.service';
 
-describe('NavbarTopComponent', () => {
+fdescribe('NavbarTopComponent', () => {
   let component: NavbarTopComponent;
   let fixture: ComponentFixture<NavbarTopComponent>;
-
+  let store: Store<any>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarTopComponent ]
+      declarations: [ NavbarTopComponent ],
+      imports:
+      [
+        StoreModule.forRoot({State: reducer}),
+        ComponentsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers:[
+        MessageService      
+      ]
     })
     .compileComponents();
   }));
